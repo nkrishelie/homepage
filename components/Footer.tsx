cfa409b0c5e8433f4d70dcd8e7360cd25b291725
nkrishelie <60033261+nkrishelie@users.noreply.github.com>
2026-01-29 17:44:23 +0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,11 +3,12 @@ import { useLanguage } from '../LanguageContext';
 
 export const Footer: React.FC = () => {
   const { content } = useLanguage();
+
   return (
     <footer className="bg-white py-12 border-t border-academic-200">
       <div className="container mx-auto px-6 flex flex-col md:flex-row justify-between items-center text-sm text-academic-500">
         <div className="mb-4 md:mb-0">
-          <p>&copy; {new Date().getFullYear()} {content.personal.name}. Все права защищены.</p>
+          <p>&copy; {new Date().getFullYear()} {content.personal.name}. {content.ui.footer.rights}</p>
         </div>
         
         <div className="flex space-x-6">
